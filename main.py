970c8efc89a2df1076c188e1dbc8919c2d6e3722
JunrichMenardo <junrich.menardo@urios.edu.ph>
2026-01-19 15:27:27 +0800

    Fixed Mike's syntax and finalized group code

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ def main():
     # CONTRIBUTOR 2 (Classmate A)
     # -----------------------------------------
     # TODO: Classmate A, please uncomment the line below and add your function
-     print("Contributor 2: [Mike gwapo] says hello!")
+    print("Contributor 2: Mike gwapo says hello!")
 
     # -----------------------------------------
     # CONTRIBUTOR 3 (Classmate B)
     # -----------------------------------------
     # TODO: Classmate B, please uncomment the line below and add your function
-    print("Contributor 3: [LM] likes Python!")
+    print("Contributor 3: LM likes Python!")
 
     # -----------------------------------------
     # CONTRIBUTOR 4 (Classmate C)
     # -----------------------------------------
     # TODO: Classmate C, please uncomment the line below and add your function
-    print("Contributor 4: [Mats] is here!")
+    print("Contributor 4: Mats is here!")
 
 if __name__ == "__main__":
     main()
